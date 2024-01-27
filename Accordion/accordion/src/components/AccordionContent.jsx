caeb860e5a5a4cb3b0a1e0b2e7425e193571aa4d
amithkashyap <amithkashyaprv@gmail.com>
2024-01-28 01:25:43 +0530

    refcatored Accordion content component

--- Accordion/accordion/src/components/AccordionContent.jsx
+++ Accordion/accordion/src/components/AccordionContent.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const AccordionContemt = ({ data }) => {
   return (
-    <div className="px-10 py-5">
-      <h1 className="text-2xl py-2">{data}</h1>
-      <h3 className="leading-loose">
+    <div className="px-10 py-1 mb-2">
+      <h1 className="text-lg py-1">{data}</h1>
+      <h3 className="leading-loose pb-4">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti,
         eveniet molestiae. Maiores earum harum accusantium aut. Nisi
         exercitationem ratione sunt fugit eos, libero asperiores fugiat

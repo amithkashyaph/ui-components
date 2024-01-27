2e73b9fdea5bbc47b981a677ac071661f727beb2
amithkashyap <amithkashyaprv@gmail.com>
2024-01-28 00:55:07 +0530

    added Accordion content

---
+++ Accordion/accordion/src/components/AccordionContent.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const AccordionContemt = ({ data }) => {
+  return (
+    <div className="px-10 py-5">
+      <h1 className="text-2xl py-2">{data}</h1>
+      <h3 className="leading-loose">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corrupti,
+        eveniet molestiae. Maiores earum harum accusantium aut. Nisi
+        exercitationem ratione sunt fugit eos, libero asperiores fugiat
+        cupiditate, quam similique natus enim.
+      </h3>
+    </div>
+  );
+};
+
+export default AccordionContemt;

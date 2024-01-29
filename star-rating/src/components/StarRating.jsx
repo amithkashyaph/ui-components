0332cb1485c141517f6aa3b1d39de9c3a7150b64
amithkashyap <amithkashyaprv@gmail.com>
2024-01-30 00:17:19 +0530

    modified Star rating component

--- star-rating/src/components/StarRating.jsx
+++ star-rating/src/components/StarRating.jsx
@@ -1,22 +1,35 @@
 import React, { useState } from "react";
 import Star from "./Star";
 
-const StarRating = ({ maxRating, color }) => {
+const StarRating = ({ maxRating, color, messages }) => {
   const [selectedStarRating, setSelectedStarRating] = useState(0);
   const [tempRating, setTempRating] = useState(0);
 
   return (
-    <div className="flex gap-4 mt-14 mx-auto w-[720px] justify-center">
-      {Array.from({ length: maxRating }, (_, i) => (
-        <Star
-          key={i}
-          full={tempRating ? tempRating >= i + 1 : selectedStarRating >= i + 1}
-          index={i + 1}
-          onRatingSelect={setSelectedStarRating}
-          onHover={setTempRating}
-          color={color}
-        />
-      ))}
+    <div className="flex gap-10  mt-14 mx-auto w-[720px]  items-center">
+      <div className="flex gap-4">
+        {Array.from({ length: maxRating }, (_, i) => (
+          <Star
+            key={i}
+            full={
+              tempRating ? tempRating >= i + 1 : selectedStarRating >= i + 1
+            }
+            index={i + 1}
+            onRatingSelect={setSelectedStarRating}
+            onHover={setTempRating}
+            color={color}
+          />
+        ))}
+      </div>
+      <div>
+        {messages?.length === maxRating ? (
+          <h1 className="text-2xl">
+            {messages[tempRating ? tempRating - 1 : selectedStarRating - 1]}
+          </h1>
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 };
